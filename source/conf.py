d07bb175f325c15146603d8efb6f9c2d08e5dd4d
rstone <rstoneba@utdallas.edu>
2016-01-14 18:21:10 -0600

    Updated title page for latex

--- source/conf.py
+++ source/conf.py
@@ -244,7 +244,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'pysat.tex', u'pysat Documentation',
+  (master_doc, 'pysat.tex', u'Python Satellite Analysis Toolkit (pysat) Documentation',
    u'Russell Stoneback', 'manual'),
 ]
 
